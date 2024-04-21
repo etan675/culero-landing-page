6f819b621d0bfd629a2934a266bd85d84c699a0c
Eric Tang <me@Erics-MacBook-Pro.local>
2024-04-21 18:38:20 +1000

    improve footer format

--- src/js/components/LandingFooter.js
+++ src/js/components/LandingFooter.js
@@ -12,32 +12,26 @@ const LandingFooter = () => {
                 How It Works
             </div>
             <div className="landing-footer-content">
-                <div className="landing-footer-content-block">
-                    <div>
-                        <PersonIcon />
-                        <div>Discover</div>
-                    </div>
-                    <div className="landing-footer-content-block-text">
-                        Take the time to self reflect & review the feedback people are leaving about you.
-                    </div>
+                <div className="landing-footer-content-block-header">
+                    <PersonIcon />
+                    <div>Discover</div>
                 </div>
-                <div className="landing-footer-content-block">
-                    <div>
-                        <WriteIcon />
-                        <div>Review others</div>
-                    </div>
-                    <div className="landing-footer-content-block-text">
-                        Write reviews based on your experience & interactions.
-                    </div>
+                <div className="landing-footer-content-block-header">
+                    <WriteIcon />
+                    <div>Review others</div>
                 </div>
-                <div className="landing-footer-content-block">
-                    <div>
-                        <PeopleIcon />
-                        <div>Make better choices</div>
-                    </div>
-                    <div className="landing-footer-content-block-text">
-                        Use the platform to research peers before choosing to work with them.
-                    </div>
+                <div className="landing-footer-content-block-header">
+                    <PeopleIcon />
+                    <div>Make better choices</div>
+                </div>
+                <div className="landing-footer-content-block-text">
+                    Take the time to self reflect & review the feedback people are leaving about you.
+                </div>
+                <div className="landing-footer-content-block-text">
+                    Write reviews based on your experience & interactions.
+                </div>
+                <div className="landing-footer-content-block-text">
+                    Use the platform to research peers before choosing to work with them.
                 </div>
             </div>
             <div className="landing-footer-line-break"><hr /></div>
